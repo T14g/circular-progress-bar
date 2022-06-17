3d70e8e32f459dbb516783c9e64b2a4836c8cab6
Tiago <ottiagos@gmail.com>
2022-06-16 23:52:01 -0300

    Two more circles

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import CircularProgressBar from "./components/CircularProgressBar";
 
 const App = () => {
   return(
-    <CircularProgressBar 
+    <div>
+      <CircularProgressBar 
       size={100}
       progress={70}
       strokeWidth={10}
       circleOneStroke='#fff'
       circleTwoStroke='red'
     />
+    <CircularProgressBar 
+      size={200}
+      progress={35}
+      strokeWidth={20}
+      circleOneStroke='#fff'
+      circleTwoStroke='red'
+    />
+    <CircularProgressBar 
+      size={100}
+      progress={88}
+      strokeWidth={10}
+      circleOneStroke='#fff'
+      circleTwoStroke='red'
+    />
+    </div>
   )
 };
 
